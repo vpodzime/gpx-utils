2cb0c9f7827945812645c5a6e040b5ac3d17784a
Vratislav Podzimek <v.podzimek@mykolab.com>
2017-11-22 21:26:11 +0100

    Use DOM parser instead of the custom SAX-based one
    We will need to modify the tree so we need its DOM representation
    anyway.

--- src/gpx_elevator.adb
+++ src/gpx_elevator.adb
@@ -1,16 +1,22 @@
 with Sax.Readers;        use Sax.Readers;
+with DOM.Readers;        use DOM.Readers;
+with DOM.Core;           use DOM.Core;
+with DOM.Core.Documents; use DOM.Core.Documents;
+with DOM.Core.Nodes;     use DOM.Core.Nodes;
+with DOM.Core.Attrs;     use DOM.Core.Attrs;
 with Input_Sources.File; use Input_Sources.File;
-with GPX_Reader;         use GPX_Reader;
 with Ada.Text_IO;
 
 procedure GPX_Elevator is
    package T_IO renames Ada.Text_IO;
 
-   Reader : GPX_Reader.Reader;
    Input  : File_Input;
-
-   function Get_Pid return Integer;
-   pragma Import(C, Get_Pid, "getpid");
+   Reader : Tree_Reader;
+   Doc    : Document;
+   List   : Node_List;
+   N      : Node;
+   A      : Attr;
+   Count  : Natural := 0;
 
 begin
    Set_Public_Id (Input, "TomTom GPX data");
@@ -23,16 +29,21 @@ begin
    Parse (Reader, Input);
    Close (Input);
 
-   for Pos of Reader.Points loop
-      T_IO.Put ("Got point: ");
-      T_IO.Put (Coord'Image (Pos.Lon) & ", ");
-      T_IO.Put_Line (Coord'Image (Pos.Lat));
-   end loop;
-   T_IO.Put_Line ("Number of points: " & Natural'Image (Reader.Get_N_Points));
+   Doc := Get_Tree (Reader);
 
-   --  T_IO.Put_Line ("PID: " & Integer'Image(Get_Pid));
-   --  loop
-   --     delay 10_000.0;
-   --  end loop;
+   List := Get_Elements_By_Tag_Name (Doc, "trkpt");
+   for Index in 1 .. Length (List) loop
+      N := Item (List, Index - 1);
+      A := Get_Named_Item (Attributes (N), "lon");
+      T_IO.Put ("Got point: ");
+      T_IO.Put (Value (A) & ", ");
+      A := Get_Named_Item (Attributes (N), "lat");
+      T_IO.Put_Line (Value (A));
+      Count := Count + 1;
+   end loop;
+   T_IO.Put_Line ("Number of points: " & Natural'Image (Count));
+
+   Free (List);
+   Free (Reader);
 
 end GPX_Elevator;
