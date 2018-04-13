1fefde560946ba4d6a25cfe6c645e101f2924de1
Vratislav Podzimek <v.podzimek@mykolab.com>
2018-04-13 15:41:08 +0200

    Make sure only space is removed from Coord'Image on concatenation
    If there is some other character, we need to preserve it.

--- src/position_types.adb
+++ src/position_types.adb
@@ -2,13 +2,24 @@ package body Position_Types is
    function "&" (Pre : String; C : Coord) return String is
       Img : String := Coord'Image(C);
    begin
-      return Pre & Img(2..Img'Length);
+      --  remove the leading space if any (instead of the '-' char)
+      if Img'Length > 0 and then Img(Img'First) = ' ' then
+         return Pre & Img(2..Img'Length);
+      else
+         return Pre & Img;
+      end if;
    end;
 
    function "&" (Pre : Ada.Strings.Unbounded.Unbounded_String; C : Coord) return Ada.Strings.Unbounded.Unbounded_String is
+      use Ada.Strings.Unbounded;
       use type Ada.Strings.Unbounded.Unbounded_String;
       Img : String := Coord'Image(C);
    begin
-      return Pre & Img(2..Img'Length);
+      --  remove the leading space if any (instead of the '-' char)
+      if Img'Length > 0 and then Img(Img'First) = ' ' then
+         return Pre & Img(2..Img'Length);
+      else
+         return Pre & Img;
+      end if;
    end;
 end Position_Types;
