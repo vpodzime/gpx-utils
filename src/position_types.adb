052d7be7405f5416c0e1863c0961929decdb632e
Vratislav Podzimek <v.podzimek@mykolab.com>
2018-04-13 15:45:01 +0200

    Add the Elevation to the Point record
    So that we can store it analyze it later.

--- src/position_types.adb
+++ src/position_types.adb
@@ -22,4 +22,29 @@ package body Position_Types is
          return Pre & Img;
       end if;
    end;
+
+   function "&" (Pre : String; E : Elevation_Type) return String is
+      Img : String := Elevation_Type'Image(E);
+   begin
+      --  remove the leading space if any (instead of the '-' char)
+      if Img'Length > 0 and then Img(Img'First) = ' ' then
+         return Pre & Img(2..Img'Length);
+      else
+         return Pre & Img;
+      end if;
+   end;
+
+   function "&" (Pre : Ada.Strings.Unbounded.Unbounded_String; E : Elevation_Type)
+                return Ada.Strings.Unbounded.Unbounded_String is
+      use Ada.Strings.Unbounded;
+      use type Ada.Strings.Unbounded.Unbounded_String;
+      Img : String := Elevation_Type'Image(E);
+   begin
+      --  remove the leading space if any (instead of the '-' char)
+      if Img'Length > 0 and then Img(Img'First) = ' ' then
+         return Pre & Img(2..Img'Length);
+      else
+         return Pre & Img;
+      end if;
+   end;
 end Position_Types;
--- src/position_types.ads
+++ src/position_types.ads
@@ -3,13 +3,19 @@ with Ada.Strings.Unbounded;
 
 package Position_Types is
    type Coord is delta 0.000001 digits 8;
+   type Elevation_Type is delta 0.000001 digits 10; --  -9999..9999
 
    function "&" (Pre : String; C : Coord) return String;
    function "&" (Pre : Ada.Strings.Unbounded.Unbounded_String; C : Coord) return Ada.Strings.Unbounded.Unbounded_String;
 
+   function "&" (Pre : String; E : Elevation_Type) return String;
+   function "&" (Pre : Ada.Strings.Unbounded.Unbounded_String; E : Elevation_Type)
+                return Ada.Strings.Unbounded.Unbounded_String;
+
    type Position is record
       Lon: Coord;
       Lat: Coord;
+      Elevation: Elevation_Type;
    end record;
 
    package Position_Vector is new Ada.Containers.Vectors(Natural, Position);
