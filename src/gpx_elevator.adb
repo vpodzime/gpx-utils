dc399ca3f769ff017241046212a14b5b80087f8d
Vratislav Podzimek <v.podzimek@mykolab.com>
2018-04-28 14:28:31 +0200

    Pass the path to a GPX file as an argument
    Instead of using the hardcoded file name/path.

--- src/gpx_elevator.adb
+++ src/gpx_elevator.adb
@@ -1,6 +1,7 @@
 with Ada.Text_IO;
 with Ada.IO_Exceptions;
 with GNAT.OS_Lib;
+with GNAT.Command_Line;
 
 with Elevator;
 with GPX_Reader;
@@ -10,7 +11,13 @@ procedure GPX_Elevator is
    use Ada.Text_IO;
    Reader : GPX_Reader.Reader;
    File : File_Type;
+   GPX_File_Path: String := GNAT.Command_Line.Get_Argument;
 begin
+   if GPX_File_Path'Length = 0 then
+      Put_Line("Path to a GPX file required");
+      GNAT.OS_Lib.OS_Exit (1);
+   end if;
+
    begin
       Open (File => File,
             Mode => In_File,
@@ -25,7 +32,7 @@ begin
       API_Key : String := Get_Line (File);
       use Position_Types;
    begin
-      Reader.Read_Points ("sample.gpx");
+      Reader.Read_Points (GPX_File_Path);
       Elevator.Elevate_Points (Reader.Points, API_Key);
       for Point of Reader.Points loop
          Put_Line("Point: " & Point);
